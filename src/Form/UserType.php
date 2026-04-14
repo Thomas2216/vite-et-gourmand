3fb9905404a64df1e818890d81b911afa1e20aed
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-14 15:16:11 +0200

    fix validation usertype

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,29 +4,78 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-        {
+    {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('telephone')
-            ->add('adresse')
-            ->add('ville')
+            ->add('nom', TextType::class, [
+                'constraints' => [
+                    new NotBlank(message: 'Le nom est obligatoire.'),
+                    new Length(max: 255, maxMessage: 'Le nom ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank(message: 'Le prénom est obligatoire.'),
+                    new Length(max: 255, maxMessage: 'Le prénom ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(message: 'L\'adresse email est obligatoire.'),
+                    new Email(message: 'L\'adresse email « {{ value }} » n\'est pas valide.'),
+                    new Length(max: 180, maxMessage: 'L\'email ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'constraints' => [
+                    new NotBlank(message: 'Le mot de passe est obligatoire.'),
+                    new Length(
+                        min: 8,
+                        max: 4096,
+                        minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
+                    ),
+                ],
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone',
+                'constraints' => [
+                    new NotBlank(message: 'Le numéro de téléphone est obligatoire.'),
+                    new Length(max: 20, maxMessage: 'Le téléphone ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse',
+                'constraints' => [
+                    new NotBlank(message: 'L\'adresse est obligatoire.'),
+                    new Length(max: 255, maxMessage: 'L\'adresse ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
+            ->add('ville', TextType::class, [
+                'constraints' => [
+                    new NotBlank(message: 'La ville est obligatoire.'),
+                    new Length(max: 100, maxMessage: 'La ville ne peut pas dépasser {{ limit }} caractères.'),
+                ],
+            ])
         ;
     }
 
-        public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class
+            'data_class' => User::class,
         ]);
     }
 }
