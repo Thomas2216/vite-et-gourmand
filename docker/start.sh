fea9d597283325ad8d510e1b431435c1b9b5ef75
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 14:10:21 +0200

    fix config docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "PORT is: $PORT"
 sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
-echo "Nginx config:"
-cat /etc/nginx/conf.d/default.conf
 php-fpm -D
+sleep 2
 nginx -g "daemon off;"
