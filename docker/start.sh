f7244f14608f88b4072fd8e0458f3852f2180329
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 18:04:36 +0200

    chore: nettoyage fichiers Railway, Dockerfile renommé en Dockerfile.dev

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p /var/www/html/var/cache/prod/pools
-mkdir -p /var/www/html/var/log
-chmod -R 777 /var/www/html/var
-php bin/console cache:warmup --env=prod 2>/dev/null || true
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod 2>/dev/null || true
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
