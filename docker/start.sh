2a9603b4c8c24298a17dbd7b164a082f07cdcac3
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 17:12:20 +0200

    reconfig docker pour déploiement

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-mkdir -p /var/www/html/var/cache/prod
+mkdir -p /var/www/html/var/cache/prod/pools
 mkdir -p /var/www/html/var/log
 chmod -R 777 /var/www/html/var
 php bin/console cache:warmup --env=prod 2>/dev/null || true
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod 2>/dev/null || true
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
