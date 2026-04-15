6dc113a79a4b61e5465716642ce6293fb9e4f835
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 17:34:18 +0200

    reconfig docker pour déploiement

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,8 @@ mkdir -p /var/www/html/var/cache/prod/pools
 mkdir -p /var/www/html/var/log
 chmod -R 777 /var/www/html/var
 chown -R www-data:www-data /var/www/html/var
+
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 php bin/console cache:warmup --env=prod 2>/dev/null || true
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod 2>/dev/null || true
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
