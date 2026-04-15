1651bf03a1bc9dbe788857de36076bfaeb9153a6
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 16:45:57 +0200

    reconfig docker pour déploiement

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
+mkdir -p /var/www/html/var/cache/prod
+mkdir -p /var/www/html/var/log
+chmod -R 777 /var/www/html/var
 php bin/console cache:warmup --env=prod 2>/dev/null || true
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
