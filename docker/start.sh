a3d62ff451c4b96b33cd0dcce7a257003f89a27e
Thomas2216 <valle_thomas@hotmail.fr>
2026-04-15 15:50:24 +0200

    reconfig docker pour déploiement

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+echo "=== START SCRIPT ==="
+echo "PORT: $PORT"
 sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
+echo "=== NGINX CONFIG ==="
+cat /etc/nginx/conf.d/default.conf
+echo "=== TESTING NGINX ==="
+nginx -t 2>&1
+echo "=== STARTING PHP-FPM ==="
 php-fpm -D
 sleep 2
+echo "=== STARTING NGINX ==="
 nginx -g "daemon off;"
